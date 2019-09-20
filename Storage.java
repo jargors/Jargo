1523f148734d4c1e8e96df9875b4d8d24c12b42a
James Pan <jamesjpan@outlook.com>
2019-09-20 16:18:20 +0800

    add DBUpdateServerRoute(2)

--- Storage.java
+++ Storage.java
@@ -622,6 +622,47 @@
         throw new RuntimeException("database failure");
       }
     }
+    public void DBUpdateServerRoute(int sid, int[] route)
+    throws RuntimeException {
+      int[] output;
+      int se, sq;
+      try {
+        output = DBFetch(48, 2, sid);
+        se = output[0];
+        sq = output[1];
+        PSClear(76);
+        PSAdd(76, sid, route[0]);
+        PSSubmit(76);
+        PSClear(10);
+        for (int i = 0; i < (route.length - 3); i += 2) {
+          int t1 = route[i];
+          int v1 = route[(i + 1)];
+          int t2 = route[(i + 2)];
+          int v2 = route[(i + 3)];
+          output = DBFetch(46, 2, v1, v2);
+          int dd = output[0];
+          int nu = output[1];
+          PSAdd(10, uid, se, t1, v1, t2, v2, dd, nu);
+        }
+        PSSubmit(10);
+        PSClear(77);
+        int te = route[(route.length - 2)];
+        int ve = route[(route.length - 1)];
+        PSAdd(77, te, ve, sid);
+        PSSubmit(77);
+        conn.commit();
+      }
+      catch (SQLException e1) {
+        printSQLException(e1);
+        try {
+          conn.rollback();
+        } catch (SQLException e2) {
+          printSQLException(e2);
+        }
+        DBSaveBackup("db-lastgood");
+        throw new RuntimeException("database failure");
+      }
+    }
     public void DBRearrangeJobs(int sid, int[] sched, int[] route)
     throws RuntimeException {
       int[] output;
@@ -647,8 +688,8 @@
         }
         PSSubmit(10);
         PSClear(77);
-        int te = sched[(sched.length - 3)];
-        int ve = sched[(sched.length - 2)];
+        int te = route[(route.length - 2)];
+        int ve = route[(route.length - 1)];
         PSAdd(77, te, ve, sid);
         PSSubmit(77);
         int bound = ((sched.length/3) - 1);
@@ -735,8 +776,8 @@
         }
         PSSubmit(10);
         PSClear(77);
-        int te = sched[(sched.length - 3)];
-        int ve = sched[(sched.length - 2)];
+        int te = route[(route.length - 2)];
+        int ve = route[(route.length - 1)];
         PSAdd(77, te, ve, sid);
         PSSubmit(77);
         int bound = ((sched.length/3) - 1);
@@ -861,8 +902,8 @@
         }
         PSSubmit(10);
         PSClear(77);
-        int te = sched[(sched.length - 3)];
-        int ve = sched[(sched.length - 2)];
+        int te = route[(route.length - 2)];
+        int ve = route[(route.length - 1)];
         PSAdd(77, te, ve, sid);
         PSSubmit(77);
         int bound = ((sched.length/3) - 1);
