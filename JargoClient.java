2c5e58a77c876e33627310bd7363e9dd4e2e9f80
James Pan <jamesjpan@outlook.com>
2019-10-05 09:37:05 +0800

    move out setStorageInterface(1); change private to protected

--- JargoClient.java
+++ JargoClient.java
@@ -2,14 +2,12 @@
   import com.github.jargors.SimulationInterface;
   import java.time.LocalDateTime;
   public abstract class JargoClient {
-    private int[] requests;
-    private int[] locations;
-    private int r_collection_period = 1;
-    private int s_collection_period = 1;
-    private SimulationInterface simulator;
-    public JargoClient(SimulationInterface src) {
-      simulator = src;
-    }
+    protected int[] requests;
+    protected int[] locations;
+    protected int r_collection_period = 1;
+    protected int s_collection_period = 1;
+    protected SimulationInterface simulator;
+    public JargoClient() { }
       public void collectRequests(int[] src) {
         requests = src.clone();
         endCollectRequests();
@@ -17,6 +15,9 @@
       public void collectServerLocations(int[] src) {
         locations = src.clone();
         endCollectServerLocations();
+      }
+      public void setSimulationInterface(SimulationInterface src) {
+        simulator = src;
       }
       public int getRequestCollectionPeriod() {
         return r_collection_period;
@@ -30,7 +31,7 @@
       public void setServerLocationCollectionPeriod(int t) {
         s_collection_period = t;
       }
-      private void endCollectRequests() {
+      protected void endCollectRequests() {
         for (int i = 0; i < (requests.length - 6); i += 7) {
           handleRequest(new int[] {
             requests[i],
@@ -43,7 +44,7 @@
           });
         }
       }
-      private void endCollectServerLocations() {
+      protected void endCollectServerLocations() {
         for (int i = 0; i < (locations.length - 2); i += 3) {
           handleServerLocation(new int[] {
             locations[i],
@@ -52,13 +53,13 @@
           });
         }
       }
-      private void handleRequest(int[] r) {
+      protected void handleRequest(int[] r) {
         Print("handleRequest(1) got Request "+r[0]);
       }
-      private void handleServerLocation(int[] loc) {
+      protected void handleServerLocation(int[] loc) {
         Print("handleServerLocation(1) got Server "+loc[0]);
       }
-      private void Print(String msg) {
+      protected void Print(String msg) {
         System.out.println("[JargoClient]["+LocalDateTime.now()+"]"
           + "[t="+simulator.getSimulationWorldTime()+"] "+msg);
       }
