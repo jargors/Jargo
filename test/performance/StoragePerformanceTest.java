f09bfabab3f8ee159b903bc8f89354914f2e7c1b
James Pan <jamesjpan@outlook.com>
2019-10-26 11:10:36 +0800

    add perf tests for DBQueryQueuedRequests(1), DBQueryServerLocationsActive(1)

--- test/performance/StoragePerformanceTest.java
+++ test/performance/StoragePerformanceTest.java
@@ -6,6 +6,36 @@ public class StoragePerformanceTest {
     Print("Starting storage performance tests");
     Storage storage = new Storage();
     storage.DBLoadBackup("data/db");
+    {
+      long _t0 = System.currentTimeMillis();
+      int _count = 0;
+      float _dur = 0;
+      int[] output = new int[] { };
+      for (int t = 0; t < 1800; t++) {
+        output = storage.DBQueryQueuedRequests(t);
+        System.out.print("\r          \r"+_count);
+        _count++;
+      }
+      long _t1 = System.currentTimeMillis();
+      _dur=((_t1 - _t0)/(float)(_count == 0 ? 1 : _count));
+      System.out.print("\r");
+      Print("DBQueryQueuedRequests(1): "+_dur+" ms/call");
+    }
+    {
+      long _t0 = System.currentTimeMillis();
+      int _count = 0;
+      float _dur = 0;
+      int[] output = new int[] { };
+      for (int t = 0; t < 1800; t++) {
+        output = storage.DBQueryServerLocationsActive(t);
+        System.out.print("\r          \r"+_count);
+        _count++;
+      }
+      long _t1 = System.currentTimeMillis();
+      _dur=((_t1 - _t0)/(float)(_count == 0 ? 1 : _count));
+      System.out.print("\r");
+      Print("DBQueryServerLocationsActive(1): "+_dur+" ms/call");
+    }
     Print("Complete!");
   }
   private static void Print(String msg) {
