c8b2ac1a8338d421c6ec509bdaf99de87ee65c37
James Pan <jamesjpan@outlook.com>
2019-10-29 09:16:52 +0800

    add test _getConnection(0)

--- test/performance/StoragePerformanceTest.java
+++ test/performance/StoragePerformanceTest.java
@@ -1,11 +1,33 @@
 import com.github.jargors.Storage;
 import java.time.LocalDateTime;
+import java.sql.Connection;
 public class StoragePerformanceTest {
   
   public static void main(String[] args) {
     Print("Starting storage performance tests");
     Storage storage = new Storage();
     storage.DBLoadBackup("data/db");
+    {
+      final int n = 100000;  // 100,000
+      long _t0 = System.currentTimeMillis();
+      int _count = 0;
+      float _dur = 0;
+      Connection[] arr = new Connection[n];
+      for (int i = 0; i < n; i++) {
+        arr[i] = storage._getConnection();
+        System.out.print("\r          \r"+_count);
+        _count++;
+      }
+      long _t1 = System.currentTimeMillis();
+      _dur=((_t1 - _t0)/(float)(_count == 0 ? 1 : _count));
+      System.out.print("\r");
+      Print("_getConnection(0): "+_dur+" ms/call");
+      for (Connection c : arr) {
+        try {
+          c.close();
+        } catch (Exception e) { }
+      }
+    }
     {
       long _t0 = System.currentTimeMillis();
       int _count = 0;
