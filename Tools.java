6ed892a16cad5a3bec75372385915c7ea7b8dde0
James Pan <jamesjpan@outlook.com>
2019-11-11 12:50:41 +0800

    cleanup

--- Tools.java
+++ Tools.java
@@ -1,9 +1,14 @@
 package com.github.jargors;
 import com.github.jargors.gtreeJNI.*;
+import com.github.jargors.exceptions.VertexNotFoundException;
+import com.github.jargors.exceptions.GtreeNotLoadedException;
+import com.github.jargors.exceptions.GtreeIllegalSourceException;
+import com.github.jargors.exceptions.GtreeIllegalTargetException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.Arrays;
 import java.time.LocalDateTime;
 import java.sql.SQLException;
+import java.io.FileNotFoundException;
 public class Tools {
   private G_Tree gtree;
   private boolean flag_gtree_loaded = false;
@@ -12,7 +17,8 @@ public class Tools {
       ConcurrentHashMap<Integer, int[]>>    lu_edges    = new ConcurrentHashMap();
   private ConcurrentHashMap<Integer, int[]> lu_users    = new ConcurrentHashMap();
   private final double CSHIFT = 10000000.0;
-    public void loadGTree(String p) {
+  private final boolean DEBUG = "true".equals(System.getProperty("jargors.tools.debug"));
+    public void loadGTree(final String p) throws FileNotFoundException {
       try {
         System.loadLibrary("gtree");
       } catch (UnsatisfiedLinkError e) {
@@ -21,53 +27,59 @@ public class Tools {
       }
       if (p.length() > 0) {
         gtreeJNI.load(p);
-        gtree = gtreeJNI.get();
-        flag_gtree_loaded = true;
+        this.gtree = gtreeJNI.get();
+        this.flag_gtree_loaded = true;
       } else {
-        System.out.println("Bad path to gtree");
+        throw new FileNotFoundException("Bad path to gtree");
       }
     }
-    public void registerVertices(ConcurrentHashMap<Integer, int[]> src) {
-      lu_vertices = src;
+    public void registerVertices(final ConcurrentHashMap<Integer, int[]> src) {
+      this.lu_vertices = src;
     }
-    public void registerEdges(ConcurrentHashMap<Integer,
+    public void registerEdges(final ConcurrentHashMap<Integer,
         ConcurrentHashMap<Integer, int[]>> src) {
-      lu_edges = src;
+      this.lu_edges = src;
     }
-    public void registerUsers(ConcurrentHashMap<Integer, int[]> src) {
-      lu_users = src;
+    public void registerUsers(final ConcurrentHashMap<Integer, int[]> src) {
+      this.lu_users = src;
     }
-    public int computeHaversine(double lng1, double lat1, double lng2, double lat2) {
-      double dlat = Math.toRadians(lat2 - lat1);
-      double dlng = Math.toRadians(lng2 - lng1);
-      double rlat1 = Math.toRadians(lat1);
-      double rlat2 = Math.toRadians(lat2);
-      double a = Math.pow(Math.sin(dlat / 2), 2)
-        + Math.pow(Math.sin(dlng / 2), 2)
-        * Math.cos(rlat1) * Math.cos(rlat2);
-      double c = 2 * Math.asin(Math.sqrt(a));
-      int d = (int) Math.round(c * 6371000);
+    public int computeHaversine(
+        final double lng1, final double lat1, final double lng2, final double lat2) {
+      final double  dlat = Math.toRadians((lat2 - lat1));
+      final double  dlng = Math.toRadians((lng2 - lng1));
+      final double rlat1 = Math.toRadians(lat1);
+      final double rlat2 = Math.toRadians(lat2);
+      final double a = Math.pow(Math.sin((dlat/2)), 2)
+        + (Math.pow(Math.sin((dlng/2)), 2)*Math.cos(rlat1)*Math.cos(rlat2));
+      final double c = 2*Math.asin(Math.sqrt(a));
+      int d = (int) Math.round(c*6371000);
       if (d == 0 && (lng1 != lng2 || lat1 != lat2)) {
         d = 1;
       }
       return d;
     }
-    public int computeHaversine(int u, int v) {
-      return computeHaversine(
-        lu_vertices.get(u)[0]/CSHIFT, lu_vertices.get(u)[1]/CSHIFT,
-        lu_vertices.get(v)[0]/CSHIFT, lu_vertices.get(v)[1]/CSHIFT);
+    public int computeHaversine(final int u, final int v) throws VertexNotFoundException {
+      if (!this.lu_vertices.containsKey(u)) {
+        throw new VertexNotFoundException("Vertex "+u+" not found.");
+      }
+      if (!this.lu_vertices.containsKey(v)) {
+        throw new VertexNotFoundException("Vertex "+v+" not found.");
+      }
+      return this.computeHaversine(
+        this.lu_vertices.get(u)[0]/CSHIFT, this.lu_vertices.get(u)[1]/CSHIFT,
+        this.lu_vertices.get(v)[0]/CSHIFT, this.lu_vertices.get(v)[1]/CSHIFT);
     }
-    public int computeDuration(int dd, int nu) {
+    public int computeDuration(final int dd, final int nu) {
       int d = (int) Math.ceil(dd/(float) nu);
       return (d == 0 ? 1 : d);
     }
-    public int[] computeShortestPath(int u, int v) {
+    public int[] computeShortestPath(final int u, final int v)
+    throws GtreeNotLoadedException, GtreeIllegalSourceException {
       int[] output = null;
-      if (!flag_gtree_loaded) {
-        throw new RuntimeException("GTree not loaded!");
+      if (!this.flag_gtree_loaded) {
+        throw new GtreeNotLoadedException("Gtree not loaded!");
       } else if (u == 0) {
-        throw new RuntimeException(
-            "Attempted to find shortest path originating from dummy vertex!");
+        throw new GtreeIllegalSourceException("Source cannot be 0!");
       } else if (v == 0) {
         output = new int[] { u, v };
       } else if (u == v) {
@@ -84,40 +96,39 @@ public class Tools {
       }
       return output;
     }
-    public int computeShortestPathDistance(int u, int v) {
+    public int computeShortestPathDistance(final int u, final int v)
+    throws GtreeNotLoadedException, GtreeIllegalSourceException {
       int d = 0;
-      if (!flag_gtree_loaded) {
-        throw new RuntimeException("GTree not loaded!");
+      if (!this.flag_gtree_loaded) {
+        throw new GtreeNotLoadedException("GTree not loaded!");
       } else if (u == 0) {
-        throw new RuntimeException(
-            "Attempted to find shortest distance originating from dummy vertex!");
+        throw new GtreeIllegalSourceException("Source cannot be 0!");
       } else if (u != v && v != 0) {
         d = gtree.search((u - 1), (v - 1));
       }
       return d;
     }
-    public int[] computeRoute(int source, int target, int starttime) {
+    public int[] computeRoute(final int source, final int target, final int starttime)
+    throws GtreeNotLoadedException, GtreeIllegalSourceException, GtreeIllegalTargetException {
       int[] output = null;
       if (source == 0) {
-        throw new RuntimeException("ERROR computeRoute(3): source cannot be 0!");
+        throw new GtreeIllegalSourceException("Source cannot be 0!");
       } else if (target == 0) {
         output = new int[] { starttime, source, starttime + 1, target };
       } else {
-        int[] path = null;
-        int[] edge = null;
-        int u, v, t;
-        path = computeShortestPath(source, target);
+        int[] path = this.computeShortestPath(source, target);
         if (path == null) {
-          throw new RuntimeException("ERROR computeRoute(3): null path!");
+          throw new GtreeIllegalTargetException("No path from source to target!");
         } else {
           output = new int[(path.length*2)];
-          output[0] = t = starttime;
+          output[0] = starttime;
           output[1] = source;
+          int t = starttime;
           int j = 2;
-          for (int i = 0; i < path.length - 1; i++) {
-            u = path[(i + 0)];
-            v = path[(i + 1)];
-            edge = lu_edges.get(u).get(v);
+          for (int i = 0; i < (path.length - 1); i++) {
+            int u = path[(i + 0)];
+            int v = path[(i + 1)];
+            int[] edge = this.lu_edges.get(u).get(v);
             output[(j + 0)] = (t += computeDuration(edge[0], edge[1]));
             output[(j + 1)] = v;
             j += 2;
@@ -126,45 +137,46 @@ public class Tools {
       }
       return output;
     }
-    public int[] filterByHaversine(int ro, int[] locs, int threshold) {
-      int n = (locs.length/3);
+    public int[] filterByHaversine(final int ro, final int[] locs, final int threshold)
+    throws VertexNotFoundException {
+      final int n = (locs.length/3);
       int[] temp = new int[n];
       int i = 0;
       for (int k = 0; k < n; k++) {
-        if (computeHaversine(ro, locs[((3*k) + 2)]) < threshold) {
+        if (this.computeHaversine(ro, locs[((3*k) + 2)]) < threshold) {
           temp[i++] = 3*k;
         }
       }
       return Arrays.copyOf(temp, i);
     }
-    public void printUser(int[] u) {
+    public void printUser(final int[] u) {
       System.out.println("User {uid="+u[0]+", q="+u[1]+", e="+u[2]+", l="+u[3]
         +", o="+u[4]+", d="+u[5]+", b="+u[6]+"}");
     }
-    public void printPath(int[] p) {
+    public void printPath(final int[] p) {
       for (Integer i : p) {
         System.out.print(i+" ");
       }
       System.out.println();
     }
-    public void printRoute(int[] w) {
+    public void printRoute(final int[] w) {
       for (int i = 0; i < (w.length - 1); i += 2) {
         System.out.print("("+w[i]+", "+w[(i + 1)]+") ");
       }
       System.out.println();
     }
-    public void printSchedule(int[] b) {
+    public void printSchedule(final int[] b) {
       for (int i = 0; i < (b.length - 3); i += 4) {
         System.out.print("("+b[i]+", "+b[(i + 1)]
           + ", "+b[(i + 2)]+", "+b[(i + 3)]+") ");
       }
       System.out.println();
     }
-  public static void Print(String a, String b) {
+  public static void Print(final String a, final String b) {
     System.out.println(String.format("[%s][%s] %s", a, LocalDateTime.now(), b));
   }
-  public static void Print(String a) {
-    Print("*", a);
+  public static void Print(final String b) {
+    System.out.println(String.format("[*][%s] %s", LocalDateTime.now(), b));
   }
   public static void PrintSQLException(SQLException e) {
     while (e != null) {
