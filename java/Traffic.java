5368b3cc8fdc7d4f0a1d382d993ffe9527a0cd48
James Pan <jamesjpan@outlook.com>
2019-11-19 13:14:39 +0800

    add newline; change apply from float to double

--- java/Traffic.java
+++ java/Traffic.java
@@ -7,9 +7,8 @@ public abstract class Traffic {
   protected Tools tools = new Tools();
   private final boolean DEBUG = "true".equals(System.getProperty("jargors.traffic.debug"));
   public Traffic() { }
-  public float apply(int v1, int v2, int t) {
-           System.out.println("Call apply("+v1+", "+v2+", "+t+")");
-           return 0;
+  public double apply(int v1, int v2, int t) {
+           return 1.0;
          }
   final public void forwardVertices(final ConcurrentHashMap<Integer, int[]> src) {
                  this.tools.registerVertices(src);
