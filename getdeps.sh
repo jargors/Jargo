2f252762b8369119d5b11fec1b7cc178db3ff187
James Pan <jamesjpan@outlook.com>
2019-11-20 20:42:44 +0800

    edit

--- getdeps.sh
+++ getdeps.sh
@@ -4,6 +4,7 @@ DBCP2=commons-dbcp2-2.7.0-bin.tar.gz
 LOGGING=commons-logging-1.2-bin.tar.gz
 POOL2=commons-pool2-2.7.0-bin.tar.gz
 
+mkdir deps
 curl https://download2.gluonhq.com/openjfx/13.0.1/$OPENJFX --output deps/$OPENJFX
 curl http://mirror.navercorp.com/apache//commons/dbcp/binaries/$DBCP2 --output deps/$DBCP2
 curl http://mirror.navercorp.com/apache//commons/logging/binaries/$LOGGING --output deps/$LOGGING
