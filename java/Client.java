33ea8c6c90fa28d2f634ad08cc2f55ca1bdc0004
James Pan <jamesjpan@outlook.com>
2019-11-26 18:15:33 +0800

    add ClientMonitor to Client

--- java/Client.java
+++ java/Client.java
@@ -6,6 +6,9 @@ import com.github.jargors.exceptions.ClientFatalException;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.io.FileNotFoundException;
+import com.github.jargors.jmx.ClientMonitor;
+import java.lang.management.*;
+import javax.management.*;
 public abstract class Client {
   protected ConcurrentLinkedQueue<int[]> queue = new ConcurrentLinkedQueue<int[]>();
   protected Communicator communicator;
@@ -15,7 +18,16 @@ public abstract class Client {
   private int stat_count_handleRequest = 0;
   private long stat_dur_total_handleRequest = 0;
   private long stat_dur_last_handleRequest = 0;
-  public Client() { }
+  public Client() {
+    try {
+      MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+      ClientMonitor mon = new ClientMonitor(this);
+      mbs.registerMBean(mon, new ObjectName("com.github.jargors.jmx:type=ClientMonitor"));
+    } catch (Exception e) {
+      System.err.printf("ClientMonitor failed; reason: %s\n", e.toString());
+      System.err.printf("Continuing with monitoring disabled\n");
+    }
+  }
   public void gtreeLoad(String p) throws FileNotFoundException {
            this.tools.GTGtreeLoad(p);
          }
