fa6ad6a7d24957fe844c010b85859a3c385b7be2
James Pan <jamesjpan@outlook.com>
2020-02-18 22:34:25 +0800

    remove useless

--- launch-cli.sh
+++ launch-cli.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
-
-## This script launches example/app
-
 _CLASSPATH=.:jar/*:dep:dep/*:example/algo/*:example/traffic/*
-if [ -z ${DERBY_PATH} ];
-    then
-        echo "Set DERBY_PATH before continuing!";
-        exit;
-    else
-        echo "Using Derby directory '$DERBY_PATH'";
-fi
 java \
     -Xmx6g \
     -Djava.library.path=dep \
